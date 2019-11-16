51b88b8aee5d190fb64226b219b3b5dc76d341d6
fgxut <fgxut1206@gmail.com>
2019-11-17 01:58:02 +0900

    [add]ユーザーの登録機能とログイン機能の追加

--- app/controllers/static_pages_controller.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class StaticPagesController < ApplicationController
-  def home
-    @user = User.new
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'static_pages#home'
+  root 'users#new'
   resources :users, only: %i[new create]
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
 end
