c94dd55d39ac1dedf6b3b6a6eff3dfd4ec0a92e5
fgxut <fgxut1206@gmail.com>
2019-12-03 23:38:31 +0900

    [fix]Postモデルのカラム名の修正 #4

--- db/migrate/20191128143420_create_posts.rb
+++ db/migrate/20191128143420_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
-      t.string :image, null: false
+      t.string :images, null: false
       t.text :content, null: false
       t.references :user, foreign_key: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2019_11_28_143420) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "image", null: false
+    t.string "images", null: false
     t.text "content", null: false
     t.bigint "user_id"
     t.datetime "created_at", null: false
