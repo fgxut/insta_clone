fb96c6fec16cfd661b84b56eae99bf5a6a6959f8
fgxut <fgxut1206@gmail.com>
2019-12-10 23:36:17 +0900

    [fix]indexアクション内のallを削除 #7

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   before_action :require_login, only: %i[new create edit update destroy]
 
   def index
-    @posts = Post.all.includes(:user).sorted.page(params[:page]).per(15)
+    @posts = Post.page(params[:page]).per(15).includes(:user).sorted
   end
 
   def show
