69255949cc18c18435c52aac38f52f7c227bf914
fgxut <fgxut1206@gmail.com>
2019-12-21 22:11:31 +0900

    [add]User/Postモデルのアソシエーションの追加 #9

--- app/models/post.rb
+++ app/models/post.rb
@@ -22,6 +22,7 @@
 
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
   scope :sorted, -> { order(created_at: :desc) }
 
   mount_uploaders :images, PostImageUploader
