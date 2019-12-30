ba5f29fed2a2ba9994aa08c2d2aec18366c9d784
fgxut <fgxut1206@gmail.com>
2019-12-31 00:03:12 +0900

    [add]LikeモデルとUser/Postモデルとの関連づけ #12

--- app/models/post.rb
+++ app/models/post.rb
@@ -23,6 +23,7 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
   
   scope :sorted, -> { order(created_at: :desc) }
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,7 @@ class User < ApplicationRecord
 
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   validates :username, uniqueness: true, presence: true
   validates :email, uniqueness: true, presence: true
